732e720edf80ed78e44437b53323ffe8b885160b
Michael B. Chun <chun_michael@ymail.com>
2014-12-16 02:51:15 -0800

    String to Phone Number Program
    Takes in a string with numbers and character and converts to all numbers
    as you would see on a phone.

---
+++ StringtoPhonenum.py
@@ -0,0 +1,76 @@
+'''Converts string to phone number format.
+eg. 435 4f3 sdwJ -> 435 433 7395
+'''
+
+from sys import version_info
+import sys
+
+def two():
+    sys.stdout.write('2')
+
+def three():
+    sys.stdout.write('3')
+
+def four():
+    sys.stdout.write('4')
+
+def five():
+    sys.stdout.write('5')
+
+def six():
+    sys.stdout.write('6')
+
+def seven():
+    sys.stdout.write('7')
+
+def eight():
+    sys.stdout.write('8')
+
+def nine():
+    sys.stdout.write('9')
+
+#converts string to number
+def getNumber(string): 
+    options = {'A' : two,
+               'B' : two,
+               'C' : two,
+               'D' : three,
+               'E' : three,
+               'F' : three,
+               'G' : four,
+               'H' : four,
+               'I' : four,
+               'J' : five,
+               'K' : five,
+               'L' : five,
+               'M' : six,
+               'N' : six,
+               'P' : seven,
+               'Q' : seven,
+               'R' : seven,
+               'S' : seven,
+               'T' : eight,
+               'U' : eight,
+               'V' : eight,
+               'W' : nine,
+               'X' : nine,
+               'Y' : nine
+        }
+    charList = list(string)
+    for index in charList:
+        if(index.isdigit() or index == '-'):
+            sys.stdout.write(index)
+        else:
+            options[index]()
+        
+
+py3 = version_info[0] > 2 #tests that Python major version > 2
+
+if py3:
+    response = input("Enter a string: ")
+    response = response.upper()
+    getNumber(response)
+else:
+    response = raw_input("Enter a string: ")
+    response = response.upper()
+    getNumber(response)
